2b96fd7554185acaeb4874713b9306a95831cb4e
GOKUL142005 <143778949+GOKUL142005@users.noreply.github.com>
2023-10-13 12:21:39 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,35 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+stdne=input("")
+stdrno=int(input(""))
+stdc=input("")
+mkpyt=int(input(""))
+mkja=int(input(""))
+mkds=int(input(""))
+mkhtml=int(input(""))
+mkcss=int(input(""))
+total=(mkpyt+mkja+mkds+mkhtml+mkcss)
+print(total)
+avg=(total/500)*100
+print(avg)
+mark=100
+if mk>=90:
+print("A+ Grade")
+print("very good")
+
+elif mk>=80:
+print(" A Grade")
+elif mk>=70:
+print("average")
+elif mk>=60:
+print("B grade")
+print("below average")
+else
+print("u grade")
+print("RA")
